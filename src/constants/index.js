4cd675cb653bbd06f336493520b91f07ba7afa1a
Jose Veliz <jose.veliz.francia@gmail.com>
2020-08-16 22:45:34 -0500

    temporal fix for prod

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,2 @@
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:9000/api/v1"
-    : "https://ires2-tesis-backend.herokuapp.com/api/v1";
+// export const BASE_URL = "http://localhost:9000/api/v1";
+export const BASE_URL = "https://ires2-tesis-backend.herokuapp.com/api/v1";
