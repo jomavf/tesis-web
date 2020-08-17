2bea3504c7f129027e5cf2d5a88e2b12ac7da26a
Jose Veliz <jose.veliz.francia@gmail.com>
2020-08-16 22:57:11 -0500

    fix prod

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:9000/api/v1";
-// export const BASE_URL = "https://ires2-tesis-backend.herokuapp.com/api/v1";
+// export const BASE_URL = "http://localhost:9000/api/v1";
+export const BASE_URL = "https://ires2-tesis-backend.herokuapp.com/api/v1";
