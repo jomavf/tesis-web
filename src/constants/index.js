3574422d31aa298530ea399acb99d3b8596b9bbc
Jose Veliz <jose.veliz.francia@gmail.com>
2020-09-06 11:25:35 -0500

    fix prod

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://localhost:9000/api/v1";
-// export const BASE_URL = "https://ires2-tesis-backend.herokuapp.com/api/v1";
+// export const BASE_URL = "http://localhost:9000/api/v1";
+export const BASE_URL = "https://ires2-tesis-backend.herokuapp.com/api/v1";
 
 export const SUCCESSFUL_OPERATION = "La operación se ha producido exitosamente";
 export const FAIL_OPERATION = "Ocurrió un error durante la operación";
